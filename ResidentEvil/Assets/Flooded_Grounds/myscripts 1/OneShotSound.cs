2d6dbd08e6f9dbad0b54d2986d83d8c4ca32e378
Gokul Singh <62161468+Goki27@users.noreply.github.com>
2023-09-09 01:31:58 +0530

    Sound work
    Finished all the sound design in the level will work tommorw on the ui and interface

---
+++ ResidentEvil/Assets/Flooded_Grounds/myscripts 1/OneShotSound.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneShotSound : MonoBehaviour
+{
+    private AudioSource myAudio;
+    private Collider col;
+    [SerializeField] bool onetime = false;
+    [SerializeField] float pauseTime = 5.0f;
+
+
+
+
+
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+        col = GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            myAudio.Play();
+            col.enabled = false;
+            if (onetime == false)
+            {
+                StartCoroutine(Resat());
+            }
+            else
+                Destroy(gameObject, pauseTime);
+        }
+    }
+
+
+    IEnumerator Resat()
+    {
+
+        yield return new WaitForSeconds(pauseTime);  
+    }
+}
